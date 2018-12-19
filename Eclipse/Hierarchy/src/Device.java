53cc097b302e59c12dd939c89d299965b1cd008a
Nicholas <nmarkou2000@gmail.com>
2018-12-19 08:52:13 -0500

    Update Device.java

--- Eclipse/Hierarchy/src/Device.java
+++ Eclipse/Hierarchy/src/Device.java
@@ -3,11 +3,15 @@ public abstract class Device {
 	private int price;
 	private String name;
 	private boolean internet;
+	private static int devicesOn;
+	private boolean isOn;
 	
 	public Device(String name, int price, boolean internet) {
 		this.name = name;
 		this.price = price;
 		this.internet = internet;
+		isOn = false;
+		
 	}
 	public int getPrice() {
 		return price;
@@ -17,6 +21,12 @@ public abstract class Device {
 	}
 	public boolean hasInternet() {
 		return internet;
+	}
+	public void turnOn() {
+		if (isOn == false) {
+			isOn = true;
+			devicesOn++;
+		}
 	}
 	@Override
 	public String toString() {
