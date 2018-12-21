ab6a9636f0437978ffef79a3576302fef2bb36c5
nMM456 <nmarkou2000@yahoo.com>
2018-12-20 19:42:21 -0500

    DONE POGU

--- Eclipse/Hierarchy/src/Laptop.java
+++ Eclipse/Hierarchy/src/Laptop.java
@@ -1,10 +1,22 @@
 
 public class Laptop extends Computer {
 	private int screen; // Screen size
+	/**
+	 * 
+	 * @param name  name of laptop
+	 * @param price  price of laptop
+	 * @param internet  does the laptop have internet
+	 * @param brand  brand of the laptop
+	 * @param screen  screen size
+	 */
 	public Laptop(String name, int price, boolean internet, String brand, int screen) {
 		super(name, price, internet, brand);
 		this.screen = screen;
 	}
+	/**
+	 * 
+	 * @return screen  the length of the screen
+	 */
 	public int getScreen() {
 		return screen;
 	}
--- Eclipse/Hierarchy/src/Printer.java
+++ Eclipse/Hierarchy/src/Printer.java
@@ -5,6 +5,11 @@ public class Printer extends Device {
 		super(name, price, internet);
 		this.paper = paper;
 	}
+	/**
+	 * 
+	 * @param pages
+	 * @return String based on if it was able to print or not.
+	 */
 	public String printPage(int pages) {
 		if (paper >= pages) {
 			paper -= pages;
@@ -14,12 +19,24 @@ public class Printer extends Device {
 			return "Not enough paper.";
 		}
 	}
+	/**
+	 * 
+	 * @return paper  returns how many pages are left
+	 */
 	public int getPaper() {
 		return paper;
 	}
+	/**
+	 * 
+	 * @param num  A phone number that you would fax to.
+	 * @return String  Returns a string that says you fax
+	 */
 	public String Fax(String num) {
 		return "You sent a Fax to " + num;
 	}
+	/**
+	 * @return String  returns a string that says you printed out a flyer.
+	 */
 	public String helpPewDiePie() {
 		return "You Printed out a subscribe to PewDiePie flyer. You did your part!";
 	}
