21c0c8f0bdf3d0ceb134ad2e516773d5503dfbdb
Nicholas <nmarkou2000@gmail.com>
2019-02-11 09:03:44 -0500

    Add files via upload

---
+++ Eclipse/MagicNumber/src/MagicSquare.java
@@ -0,0 +1,28 @@
+
+public class MagicSquare {
+	private int sideLength;
+	private int[][] Square;
+	public MagicSquare(int side) {
+		if(side%2 == 0) side++;
+		sideLength = side;
+		Square = new int [sideLength][sideLength];
+//		ACTUALLY MAKE THE CONSTRUCTOR MAKE THE SQUARE LULW
+	}
+	public MagicSquare() {
+		sideLength = 3;
+		Square = new int[sideLength][sideLength];
+//		MAKE THE CONSTRUCTOR MAKE THE SQUARE LULW
+	}
+	public int getSum() {
+		int sum = 0;
+		for(int i:Square[0]) {
+			sum = sum + i;
+		}
+		return sum;
+	}
+	
+	public int getSize() {
+		return sideLength;
+	}
+
+}
