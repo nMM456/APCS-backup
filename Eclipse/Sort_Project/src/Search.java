da2605d11bbabba3d887c07ce001aa59f3ad609c
Nicholas <Nicholas@Cindy-ASUS.westborough.local>
2019-03-07 11:55:23 -0500

    Sorting project

---
+++ Eclipse/Sort_Project/src/Search.java
@@ -0,0 +1,40 @@
+/******************************************************************
+ * A Project of Sorts
+ * 
+ * Implements Search Algorithms on integer arrays
+ * Includes Linear and Binary searches
+ * Binary Search requires a sorted list
+ * All Search methods are static
+ * All Search helper methods are private
+ * 
+ * @author 
+ *
+ *******************************************************************/
+public class Search 
+{
+	/**
+	 * Implements a Linear Search
+	 *    <Include Algorithm Steps here>
+	 * @param list - integer list to search in
+	 * @param num - integer to search for
+	 * @return index of found item, -1 if not found
+	 */
+	public static int linearSearch(int[] list, int num)
+	{
+
+	}
+	
+	/**
+	 * Implements a Binary Search
+	 *    <Include Algorithm Steps here>
+	 * Pre: list must be sorted
+	 * 
+	 * @param list - integer list to search in
+	 * @param num - integer to search for
+	 * @return index of found item, -1 if not found
+	 */
+	public static int binarySearch(int[] list, int num)
+	{
+
+	}
+}
---
+++ Eclipse/Sort_Project/src/Sort.java
@@ -0,0 +1,115 @@
+/******************************************************************
+ * A Project of Sorts
+ * 
+ * Implements Sort Algorithms on integer arrays
+ * Includes Bubble, Insertion, Selection, Merge, and Quick Sorts
+ * Merge and Quick Sorts are recursive
+ * All Sort methods are static
+ * All Sort helper methods are private
+ * 
+ * @author 
+ *
+ *******************************************************************/
+public class Sort
+{
+  
+  /************************
+   * Implements Bubble Sort
+   *  1.Scan list and compare adjacent elements
+   *  2.Swap if out of order and set a flag
+   *  3.Repeat 1-2 until no swaps are made (flag is false)
+   *    scanning one less at the end each pass
+   * Pre: unsorted array parameter
+   * Post: sorted array parameter
+   * 
+   * @param aSort integer array
+   **/
+  public static void bubbleSort(int[] aSort)
+  {
+	boolean run=true;
+	int k=0;
+	int var;
+	while (run) {
+		k++;
+		run=false;
+		for (int i=0;i<aSort.length-k;i++) {
+			if (aSort[i] > aSort[i+1]) {
+				var = aSort[i];
+				aSort[i] = aSort[i+1];
+				aSort[i+1] = var;
+				run=true;
+				}
+			}
+		}
+  }
+  
+  /************************
+   * Implements Insertion Sort
+   *  1.Start with a sorted list of one value
+   *  2.Add another value and sort the values, placing in correct location
+   *  3.Repeat 2-3 shifting sorted values to the right until it is placed
+   *    in the correct position
+   * Pre: unsorted array parameter
+   * Post: sorted array parameter
+   * 
+   * @param aSort integer array
+   **/
+  public static void insertionSort(int[] aSort)
+  {
+
+  }
+  
+  /************************
+   * Implements Selection Sort
+   *  1.Scan the list to find the smallest value
+   *  2.Swap with first position
+   *  3.Repeat 1-2 Scanning and swapping with the next position in the list
+   *    Until you are at the end of the list
+   * Pre: unsorted array parameter
+   * Post: sorted array parameter
+   * 
+   * @param aSort integer array
+   **/
+  public static void selectionSort(int[] aSort)
+  {
+
+  }
+  
+  /************************
+   * Implements Merge Sort recursively
+   *  1.Divide the list in half
+   *  2.Sort each half (recursively)
+   *  3.Merge the two sorted lists
+   *  Note: Use indexes into the array to mark two sublists
+   *        and a temporary array when merging
+   * Pre: unsorted array parameter
+   * Post: sorted array parameter
+   * 
+   * @param aSort integer array
+   **/
+  public static void mergeSort(int[] aSort)
+  {
+    //Sort the entire list using a recursive merge sort method
+    doMergeSort(aSort, 0, aSort.length-1);
+  }
+  
+  /************************
+   * Implements Quick Sort recursively
+   *  1.Divide into two sublists, < and > a pivot value
+   *  2.Recursively sort each sublist
+   *  Use the first element in the list as the pivot
+   *  Note: Use indexes into the array to mark two sublists
+   * Pre: unsorted array parameter
+   * Post: sorted array parameter
+   * 
+   * @param aSort integer array
+   **/
+  public static void quickSort(int[] aSort)
+  {
+    //Sort the entire list using the recursive merge sort method
+    doQuickSort(aSort, 0, aSort.length-1);
+  }
+  
+  
+  
+}
