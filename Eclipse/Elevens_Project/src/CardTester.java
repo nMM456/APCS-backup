f7ff788b01a314c70622abed0d76aabd7177ad19
Nicholas <Nicholas@Cindy-ASUS.westborough.local>
2019-03-18 15:57:28 -0400

    yay

---
+++ Eclipse/Elevens_Project/src/CardTester.java
@@ -0,0 +1,18 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card c1 = new Card("A", "C", 1);
+		Card c2 = new Card("A", "C", 1);
+		Card c3 = new Card("A", "B", 2);
+		System.out.println(c1.matches(c2));
+		System.out.println(c2.matches(c3));
+	}
+}
---
+++ Eclipse/apcsshortanswer/src/apcsshortanswer/dfaf.java
@@ -0,0 +1,5 @@
+package apcsshortanswer;
+
+public class dfaf {
+
+}
