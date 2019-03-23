7628d3278c48c0628706f1566b99eeef4979588a
nMM456 <nmarkou2000@gmail.com>
2019-03-22 21:30:27 -0400

    Update.

--- Eclipse/OWL_testing/OWL.py
+++ Eclipse/OWL_testing/OWL.py
@@ -4,7 +4,16 @@ url = 'https://api.overwatchleague.com/stats/players'
 url_get = requests.get(url)
 data = url_get.json()
 f = open("stats.csv", "w")
+for i in data["data"][0]:
+    if str(i) != "time_played_total":
+        f.write(str(i)+", ")
+    else:
+        f.write(str(i))
+f.write("\n")
 for x in data["data"]:
     for i in x:
-        f.write(str(x[i])+", ")
+        if str(i) != "time_played_total":
+            f.write(str(x[i])+", ")
+        else:
+            f.write(str(x[i]))
     f.write("\n")
