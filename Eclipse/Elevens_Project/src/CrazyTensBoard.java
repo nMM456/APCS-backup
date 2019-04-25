f99206b4483996e2e38f32ef4dd5c8717c11fb28
nMM456 <nmarkou2000@gmail.com>
2019-04-25 11:30:51 -0400

    work

--- Eclipse/Elevens_Project/src/CrazyTensBoard.java
+++ Eclipse/Elevens_Project/src/CrazyTensBoard.java
@@ -1,4 +1,18 @@
+import java.util.List;
 
 public class CrazyTensBoard extends TensBoard {
-
+	public CrazyTensBoard() {
+		super();
+	}
+//	If the two selected cards is equal 0, remove them.
+//	Need another rules/ideas to complete
+	@Override
+	public boolean isLegalZero(List<Integer> selectedCards) {
+		for (int i=0;i<selectedCards.size();)
+	}
+	
+	@Override
+	public boolean isLegal(List<Integer> selectedCards) {
+		return super.isLegal(selectedCards) || (selectedCards.size()==2 && cardAt
+	}
 }
