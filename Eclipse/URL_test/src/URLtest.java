c21505c94e07c720144ee735262cafb427cad80e
nMM456 <nmarkou2000@gmail.com>
2019-05-15 13:19:56 -0400

    AP test soon

--- Eclipse/URL_test/src/URLtest.java
+++ Eclipse/URL_test/src/URLtest.java
@@ -1,12 +1,14 @@
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 public class URLtest {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		URL OWL = new URL("http://api.overwatchleague.com/live-match");
-		System.out.println(OWL.getContent());
+		URLConnection web = OWL.openConnection();
 	}
 
 }
