6b8f4299d6f1e4f858e933dca16526668aadb4a2
nMM456 <nmarkou2000@gmail.com>
2019-05-16 09:02:19 -0400

    AP tomorrow oh no

--- Eclipse/URL_test/src/URLtest.java
+++ Eclipse/URL_test/src/URLtest.java
@@ -1,14 +1,21 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
+
 public class URLtest {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		URL OWL = new URL("http://api.overwatchleague.com/live-match");
-		URLConnection web = OWL.openConnection();
+		URL OWL = new URL("https://api.overwatchleague.com/live-match");
+		String data="";
+		BufferedReader read = new BufferedReader(new InputStreamReader(OWL.openStream()));
+		while (read.ready()) data = read.readLine();
+		System.out.println(data);
 	}
 
 }
