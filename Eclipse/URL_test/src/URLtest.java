f8642be45072f989ec10ee66a03f0dd8ec789671
nMM456 <nmarkou2000@gmail.com>
2019-05-31 18:27:21 -0400

    forecast

--- Eclipse/URL_test/src/URLtest.java
+++ Eclipse/URL_test/src/URLtest.java
@@ -1,23 +1,23 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Scanner;
+import org.json.JSONObject;
 
 public class URLtest {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		URL OWL = new URL("https://api.overwatchleague.com/live-match");
-		String data="";
-		BufferedReader read = new BufferedReader(new InputStreamReader(OWL.openStream()));
-		while (read.ready()) data = read.readLine();
-		int index = data.indexOf("name");
-		int index2 = data.indexOf("primaryColor");
-		System.out.println(data.substring(index+7, index2-3));
+	    String s = "https://api.overwatchleague.com/live-match";
+	    URL url = new URL(s);
+	    Scanner scan = new Scanner(url.openStream());
+	    String str = new String();
+	    while (scan.hasNext())
+	        str += scan.nextLine();
+	    scan.close();
+	    JSONObject data = new JSONObject(str);
+	    JSONObject arr = data.getJSONObject("meta");
+	    String sr = arr.getJSONObject("strings").getString("owl.live-match.now");
+	    System.out.println(sr);
 	}
 
 }
--- Eclipse/weather/src/Forecast.java
+++ Eclipse/weather/src/Forecast.java
@@ -1,6 +1,38 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Forecast {
-	public static void main(String[] args) {
-		d
+	public static void main(String[] args) throws IOException {
+	    ArrayList<String> alerts = forecast();
+	    for (String i: alerts) {
+	    	System.out.println(i);
+	    }
+	}
+	/**
+	 * Gets every alert on the weather API then returns an arraylist
+	 * with string with the description of each alert
+	 * Runs very slowly.
+	 * @return ArrayList<String> description of alerts
+	 * @throws IOException
+	 */
+	public static ArrayList<String> forecast() throws IOException {
+		ArrayList<String> alerts = new ArrayList<String>();
+	    String s = "https://api.weather.gov/alerts/active";
+	    URL url = new URL(s);
+	    Scanner scan = new Scanner(url.openStream());
+	    String str = new String();
+	    while (scan.hasNext())
+	        str += scan.nextLine();
+	    scan.close();
+	    JSONArray JSONAlert = new JSONObject(str).getJSONArray("features");
+	    for (int i=0;i<JSONAlert.length();i++) {
+	    	alerts.add(JSONAlert.getJSONObject(i).getJSONObject("properties").getString("description"));
+	    }
+	    return alerts;
 	}
 }
